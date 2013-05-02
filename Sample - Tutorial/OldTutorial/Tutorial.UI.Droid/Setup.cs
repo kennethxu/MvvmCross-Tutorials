c9282ae979220d68a10bb7ea5aa6652b8b99fb9e
Kenneth Xu <Kenneth@Xu>
2013-05-02 00:02:55 -0400

    Fixed droid and wp7 projects so they compile/deploys in Visual Studio. And most of them started up in simulator fine.

--- Sample - Tutorial/OldTutorial/Tutorial.UI.Droid/Setup.cs
+++ Sample - Tutorial/OldTutorial/Tutorial.UI.Droid/Setup.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using Android.App;
 using Android.Content;
-using Cirrious.CrossCore;
-using Cirrious.CrossCore.Platform;
 using Cirrious.MvvmCross.Droid.Platform;
-using Cirrious.MvvmCross.Binding.Droid;
-using Cirrious.MvvmCross.Plugins.Json;
 using Cirrious.MvvmCross.ViewModels;
-using Cirrious.MvvmCross.Views;
 using Tutorial.Core;
-using Tutorial.Core.Converters;
 
 namespace Tutorial.UI.Droid
 {
